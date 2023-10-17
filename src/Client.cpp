464afb7703dbf7878cd695c803127b052d326aa3
blackflame576 <pws15548@gmail.com>
2023-10-17 23:25:42 +0300

    Refactoring

--- src/Client.cpp
+++ src/Client.cpp
@@ -5,11 +5,14 @@ using namespace UNIX;
 #include "Client_POSIX.hpp"
 using namespace POSIX;
 #endif
+#include <filesystem>
 
 int main()
 {
     Client client;
     // char *data = client.OpenJSON("./toJSON.json");
-    client.SendJSON("./toJSON.json");
+    string ProjectDir = std::filesystem::current_path().generic_string();
+    string JSON_Path = ProjectDir + "/toJson.json";
+    client.SendJSON(JSON_Path);
     return 0;
 }
